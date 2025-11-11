408feb57fbaff27e2a560ca0b8464c80f258b687
MAKESWARAN-E <emakeshwaran1@gmail.com>
2025-11-12 00:18:24 +0530

    check

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "vpc_id" { value = module.networking.vpc_id }
 output "alb_dns_name" { value = module.compute.alb_dns }
 output "db_endpoint" { value = module.db.db_endpoint }
 output "ec2_public_ip" {
-  value       = module.compute.ec2_public_ip  # 🔹  Make sure your compute module exports public IP
+  value       = module.compute.ec2_public_ip  # 🔹   Make sure your compute module exports public IP
   description = "Public IP of EC2"
 }
